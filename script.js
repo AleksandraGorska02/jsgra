044ba5b3a77a140428c96dd7bd125fc1b338f4b7
“aleksandra” <aleksandra.gorska02@gmail.com>
2023-02-02 16:40:33 +0100

    m

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-function check(){
-    
-var liczba=document.getElementById("pole").value;
-if(liczba>0) document.getElementById("wynik").innerHTML="dodatnia";
-else if(liczba<0)document.getElementById("wynik").innerHTML="ujemna";
-else if (liczba==0)document.getElementById("wynik").innerHTML="zero";
-else document.getElementById("wynik").innerHTML="nie liczba";
+var numer=Math.floor(Math.random()*5)+1;
+
+var timer1=0;
+
+
+function ustawslajd(nrslajdu){
+clearTimeout(timer1);
+numer=nrslajdu-1;
+setTimeout("zmienslajd()",500);
 }
+
+
+function zmienslajd(){
+    
+numer++;if(numer>5)numer=1;
+
+var plik = "<img src=\"slajdy/" + numer + ".png\"/>";
+
+document.getElementById("slider").innerHTML=plik;
+
+
+
+timer1=setTimeout("zmienslajd()",5000);
+
+}
+
+
